48a88eec6b7f9ec95bcd447798108ebc12c964ba
Marc Van Camp <marc.vancamp@nsint.com>
2024-11-24 12:08:37 +0100

    // Unit test Sulfuras items: SellIn & quality

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -43,4 +43,16 @@ public class GildedRoseTest
         Assert.Equal(-2, Items[0].SellIn);
         Assert.Equal(50, Items[0].Quality);
     }
+    [Fact]
+    public void sulfuras()
+    {
+        // Unit test Sulfuras items: SellIn & quality
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 1, Quality = 25 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("Sulfuras, Hand of Ragnaros", Items[0].Name);
+        Assert.Equal(1, Items[0].SellIn);
+        Assert.Equal(25, Items[0].Quality);
+        app.UpdateQuality();
+    }
 }
